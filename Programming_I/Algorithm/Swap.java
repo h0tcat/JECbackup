846597434f50dce16a3b64867e6484728cd6ccb9
hayab <h0tnori@vivaldi.net>
2021-04-21 15:54:48 +0900

    first commit

---
+++ Programming_I/Algorithm/Swap.java
@@ -0,0 +1,19 @@
+public class Swap{
+   public static void main(String[] args){
+    
+    int a,b;
+    java.util.Scanner sc=new java.util.Scanner(System.in);
+    int c;
+
+    a=sc.nextInt();
+    b=sc.nextInt();
+
+    System.out.printf("%d, %d",a,b);
+
+    c=a;
+    a=b;
+    b=c;
+
+    System.out.printf("%d, %d",a,b);
+   } 
+}
